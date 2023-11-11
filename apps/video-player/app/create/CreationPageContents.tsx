ab0d1213bc16849e7717d028137fa5177de71734
Mikael <mkl.koskela@gmail.com>
2023-11-11 14:02:12 +0000

    section titles

--- apps/video-player/app/create/CreationPageContents.tsx
+++ apps/video-player/app/create/CreationPageContents.tsx
@@ -16,7 +16,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { deNoCookiefy, noCookiefy } from "@/app/components/utils";
 import UrsorInputField from "@/app/components/ursor-input-field";
 import { PALETTE } from "../../../../packages/ui/palette";
-import { UrsorButton } from "ui";
+import { Typography, UrsorButton } from "ui";
 import { Footer } from "../components/footer";
 import { HEADER_HEIGHT, Header } from "../components/header";
 
@@ -41,6 +41,18 @@ const VIDEO_HEIGHT = 475;
 // }) satisfies GetStaticProps<{
 //   videoDetails: IVideo;
 // }>;
+
+const CreationPageInputSection = (props: {
+  title: string;
+  children: React.ReactNode;
+}) => (
+  <Stack width="100%" spacing="5px" justifyContent="center" alignItems="center">
+    <Typography variant="medium" bold color="rgba(255,255,255,0.53)">
+      {props.title}
+    </Typography>
+    {props.children}
+  </Stack>
+);
 
 const extractUrl = (html: string) => html.split('src="')[1].split("?")[0];
 
@@ -111,28 +123,111 @@ function CreationPageContents(props: { details: IVideo }) {
         {!fullscreen ? (
           <Stack width={`${VIDEO_WIDTH}px`} spacing="12px">
             <Stack width="100%" position="relative" overflow="visible">
-              <Stack width="100%" spacing="5px">
-                <UrsorInputField
-                  value={title}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                    setTitle(event.target.value)
-                  }
-                  placeholder="Title"
-                  width="100%"
-                  backgroundColor={PALETTE.secondary.grey[2]}
-                  leftAlign
-                />
+              <Stack width="100%" spacing="32px">
+                <CreationPageInputSection title="Want to change the title?">
+                  <UrsorInputField
+                    value={title}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setTitle(event.target.value)
+                    }
+                    placeholder="Title"
+                    width="100%"
+                    backgroundColor={PALETTE.secondary.grey[2]}
+                    leftAlign
+                  />
+                </CreationPageInputSection>
 
-                <UrsorInputField
-                  value={description}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                    setDescription(event.target.value)
-                  }
-                  placeholder="Description"
-                  width="100%"
-                  backgroundColor={PALETTE.secondary.grey[2]}
-                  leftAlign
-                />
+                <CreationPageInputSection title="Or add your own description?">
+                  <UrsorInputField
+                    value={description}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setDescription(event.target.value)
+                    }
+                    placeholder="Description"
+                    width="100%"
+                    backgroundColor={PALETTE.secondary.grey[2]}
+                    leftAlign
+                  />
+                </CreationPageInputSection>
+
+                <CreationPageInputSection title="You can also adjust the time">
+                  <Stack direction="row" width="100%">
+                    {/* <Stack
+              height="3px"
+              width="100%"
+              bgcolor={PALETTE.secondary.orange[2]}
+            /> */}
+                    {duration && range ? (
+                      <Stack
+                        direction="row"
+                        spacing="44px"
+                        justifyContent="center"
+                        width="100%"
+                        sx={{
+                          ".MuiSlider-root": {
+                            color: "transparent !important",
+                          },
+                          ".MuiSlider-rail": {
+                            opacity: 0.4,
+                            background:
+                              "linear-gradient(90deg,#F279C5,#FD9B41)",
+                          },
+                          ".MuiSlider-track": {
+                            background:
+                              "linear-gradient(90deg,#F279C5,#FD9B41)",
+                          },
+                          ".MuiSlider-thumb": {
+                            "&:nth-of-type(3)": {
+                              background: "#F279C5",
+                            },
+                            "&:nth-of-type(4)": {
+                              background: "#FD9B41",
+                            },
+                          },
+                        }}
+                      >
+                        <DurationLabel
+                          value={range[0]}
+                          incrementCallback={() =>
+                            setRange([
+                              Math.min(duration, range[0] + 1),
+                              range[1],
+                            ])
+                          }
+                          decrementCallback={() =>
+                            setRange([Math.max(0, range[0] - 1), range[1]])
+                          }
+                        />
+                        <Slider
+                          min={0}
+                          max={duration}
+                          valueLabelDisplay="off"
+                          getAriaLabel={() => "Temperature range"}
+                          value={range}
+                          onChange={(
+                            event: Event,
+                            newValue: number | number[]
+                          ) => {
+                            setRange(newValue as number[]);
+                          }}
+                        />
+                        <DurationLabel
+                          value={range[1]}
+                          incrementCallback={() =>
+                            setRange([
+                              range[0],
+                              Math.min(duration, range[1] + 1),
+                            ])
+                          }
+                          decrementCallback={() =>
+                            setRange([range[0], Math.max(0, range[1] - 1)])
+                          }
+                        />
+                      </Stack>
+                    ) : null}
+                    {/* <Typography color={PALETTE.font.light}>{duration}</Typography> */}
+                  </Stack>
+                </CreationPageInputSection>
               </Stack>
               {/* {isCreator ? (
               <Stack
@@ -205,69 +300,7 @@ function CreationPageContents(props: { details: IVideo }) {
               </Stack>
             ) : null} */}
             </Stack>
-            <Stack direction="row">
-              {/* <Stack
-              height="3px"
-              width="100%"
-              bgcolor={PALETTE.secondary.orange[2]}
-            /> */}
-              {duration && range ? (
-                <Stack
-                  direction="row"
-                  spacing="44px"
-                  justifyContent="center"
-                  width="100%"
-                  sx={{
-                    ".MuiSlider-root": { color: "transparent !important" },
-                    ".MuiSlider-rail": {
-                      opacity: 0.4,
-                      background: "linear-gradient(90deg,#F279C5,#FD9B41)",
-                    },
-                    ".MuiSlider-track": {
-                      background: "linear-gradient(90deg,#F279C5,#FD9B41)",
-                    },
-                    ".MuiSlider-thumb": {
-                      "&:nth-of-type(3)": {
-                        background: "#F279C5",
-                      },
-                      "&:nth-of-type(4)": {
-                        background: "#FD9B41",
-                      },
-                    },
-                  }}
-                >
-                  <DurationLabel
-                    value={range[0]}
-                    incrementCallback={() =>
-                      setRange([Math.min(duration, range[0] + 1), range[1]])
-                    }
-                    decrementCallback={() =>
-                      setRange([Math.max(0, range[0] - 1), range[1]])
-                    }
-                  />
-                  <Slider
-                    min={0}
-                    max={duration}
-                    valueLabelDisplay="off"
-                    getAriaLabel={() => "Temperature range"}
-                    value={range}
-                    onChange={(event: Event, newValue: number | number[]) => {
-                      setRange(newValue as number[]);
-                    }}
-                  />
-                  <DurationLabel
-                    value={range[1]}
-                    incrementCallback={() =>
-                      setRange([range[0], Math.min(duration, range[1] + 1)])
-                    }
-                    decrementCallback={() =>
-                      setRange([range[0], Math.max(0, range[1] - 1)])
-                    }
-                  />
-                </Stack>
-              ) : null}
-              {/* <Typography color={PALETTE.font.light}>{duration}</Typography> */}
-            </Stack>
+
             <Stack pt="40px" width="100%" alignItems="center">
               <div>
                 <UrsorButton
