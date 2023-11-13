9a72384d6a5b77190b11115bea3fb1509904ef99
Mikael <mkl.koskela@gmail.com>
2023-11-13 16:58:02 +0000

    fixed description pullng

--- apps/video-player/app/create/CreationPageContents.tsx
+++ apps/video-player/app/create/CreationPageContents.tsx
@@ -112,14 +112,14 @@ function CreationPageContents(props: { details: IVideo }) {
 
   useEffect(() => {
     setPlaying(false);
-    provider === "youtube" &&
-      url &&
+    url?.includes("youtube") &&
       ApiController.getYoutubeVideoDetails(url.split("/").slice(-1)[0]).then(
         (result) => {
+          console.log(result);
           setDescription(result.description);
         }
       );
-  }, [provider, url]);
+  }, [url]);
 
   const [duration, setDuration] = useState<number | undefined>(undefined);
   const [range, setRange] = useState<number[] | undefined>(undefined);
