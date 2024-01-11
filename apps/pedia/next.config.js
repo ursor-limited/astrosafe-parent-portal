a411b81de89293865c3a999bd193c676543a18cf
Mikael <mkl.koskela@gmail.com>
2024-01-11 08:23:38 +0000

    image domains

--- apps/pedia/next.config.js
+++ apps/pedia/next.config.js
@@ -4,6 +4,12 @@ const withSvgr = require("next-svgr");
 module.exports = withSvgr({
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  images: {
+    domains: [
+      'https://www.astrosafe.co',
+      'https://astro-monorepo-pedia.vercel.app',
+    ]
+  },
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? "https://astro-monorepo-pedia.vercel.app"
