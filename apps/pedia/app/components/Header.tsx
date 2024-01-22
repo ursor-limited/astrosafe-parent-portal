c99fb5530d92ea6c1ec4d63b96b508d8ee01b1ef
Mikael <mkl.koskela@gmail.com>
2024-01-22 08:53:15 +0000

    finished popup

--- apps/pedia/app/components/Header.tsx
+++ apps/pedia/app/components/Header.tsx
@@ -4,8 +4,10 @@ import { Stack } from "@mui/system";
 import Logo from "@/images/logoWhite.svg";
 import ChevronRight from "@/images/icons/ChevronRightIcon.svg";
 import PersonIcon from "@/images/icons/PersonIcon.svg";
+import LogOutIcon from "@/images/icons/LogOutIcon.svg";
+import ListUnorderedIcon from "@/images/icons/ListUnorderedIcon.svg";
 import { PALETTE, Typography, UrsorButton } from "ui";
-import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { useState } from "react";
 import UrsorPopover from "./UrsorPopover";
 
@@ -38,9 +40,63 @@ const ProfileButton = (props: { initials: string }) => (
   </Stack>
 );
 
+const ProfilePopupButton = (props: {
+  callback: () => void;
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  text: string;
+  // hoveringOnCallback: () => void;
+  // hoveringOffCallback: () => void;
+}) => {
+  const [hovering, setHovering] = useState<boolean>(false);
+  return (
+    <Stack
+      height="36px"
+      minHeight="36px"
+      sx={{
+        cursor: "pointer",
+        "&:hover": { opacity: 0.7 },
+        transition: "0.2s",
+        svg: {
+          path: {
+            fill: hovering
+              ? PALETTE.secondary.purple[2]
+              : PALETTE.secondary.grey[5],
+          },
+        },
+      }}
+      onClick={props.callback}
+      direction="row"
+      spacing="8px"
+      alignItems="center"
+      px="20px"
+      bgcolor={hovering ? PALETTE.secondary.grey[1] : undefined}
+      // onMouseEnter={props.hoveringOnCallback}
+      // onMouseLeave={props.hoveringOffCallback}
+      onMouseEnter={() => {
+        setHovering(true);
+      }}
+      onMouseLeave={() => {
+        setHovering(false);
+      }}
+    >
+      <props.icon height="16px" width="16px" />
+      <Typography
+        variant="small"
+        bold
+        color={
+          hovering ? PALETTE.secondary.purple[2] : PALETTE.secondary.grey[5]
+        }
+      >
+        {props.text}
+      </Typography>
+    </Stack>
+  );
+};
+
 export const Header = (props: { noCreateNew?: boolean }) => {
   const { user, logout, loginWithPopup } = useAuth0();
   const [profilePopupOpen, setProfilePopupOpen] = useState<boolean>(false);
+  const [hovering, setHovering] = useState<boolean>(false);
   return (
     <Stack
       direction="row"
@@ -116,19 +172,43 @@ export const Header = (props: { noCreateNew?: boolean }) => {
             <UrsorPopover
               open={profilePopupOpen}
               content={
-                <Stack
-                  sx={{
-                    cursor: "pointer",
-                    "&:hover": { opacity: 0.7 },
-                    transition: "0.2s",
-                  }}
-                  onClick={() => logout()}
-                >
-                  <Typography bold>Log out</Typography>
+                <Stack>
+                  <Stack
+                    height="40px"
+                    alignItems="center"
+                    sx={{
+                      background: ASTRO_MAGICAL_GRADIENT,
+                      "-webkit-text-fill-color": "transparent",
+                      backgroundClip: "text",
+                      "-webkit-background-clip": "text",
+                    }}
+                    px="20px"
+                    justifyContent="center"
+                    borderBottom={`1px solid ${PALETTE.secondary.grey[2]}`}
+                  >
+                    <Typography bold variant="small">
+                      {user.email}
+                    </Typography>
+                  </Stack>
+                  <ProfilePopupButton
+                    callback={() => logout()}
+                    icon={ListUnorderedIcon}
+                    text="Dashboard"
+                    // hoveringOnCallback={() => setHovering(true)}
+                    // hoveringOffCallback={() => setHovering(true)}
+                  />
+                  <ProfilePopupButton
+                    callback={() => logout()}
+                    icon={LogOutIcon}
+                    text="Log out"
+                    // hoveringOnCallback={() => setHovering(true)}
+                    // hoveringOffCallback={() => setHovering(true)}
+                  />
                 </Stack>
               }
               closeCallback={() => setProfilePopupOpen(false)}
               placement="right"
+              noPadding
             >
               <Stack onClick={() => setProfilePopupOpen(true)}>
                 <ProfileButton
