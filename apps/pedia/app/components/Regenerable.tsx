05defca2813b4076ea378399e23980f069f9357b
Mikael <mkl.koskela@gmail.com>
2024-01-22 15:38:11 +0000

    loading byte

--- apps/pedia/app/components/Regenerable.tsx
+++ apps/pedia/app/components/Regenerable.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "@mui/system";
 import { PALETTE, UrsorButton } from "ui";
 import SyncIcon from "@/images/icons/SyncIcon.svg";
+import Byte from "./Byte";
 
 interface IRegenerableProps {
   on: boolean;
   callback: () => void;
+  loading?: boolean;
   children: React.ReactNode;
 }
 
@@ -18,10 +20,11 @@ export default function Regenerable(props: IRegenerableProps) {
       }}
       position="relative"
       borderRadius="12px"
+      overflow="hidden"
       flex={1}
     >
       {props.children}
-      {props.on ? (
+      {props.on && !props.loading ? (
         <Stack position="absolute" right="24px" top="-15px" zIndex={2}>
           <UrsorButton
             dark
@@ -35,6 +38,29 @@ export default function Regenerable(props: IRegenerableProps) {
           >
             Regenerate
           </UrsorButton>
+        </Stack>
+      ) : null}
+      {props.loading ? (
+        <Stack
+          position="absolute"
+          top={0}
+          left={0}
+          width="100%"
+          height="100%"
+          justifyContent="center"
+          alignItems="center"
+          bgcolor="rgb(255,255,255)"
+        >
+          <Stack
+            flex={1}
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              transform: "translate(-5px, -10px)",
+            }}
+          >
+            <Byte animation="loading" loop size={75} />
+          </Stack>
         </Stack>
       ) : null}
     </Stack>
