70f767ff6a6a9537f16d0f823ebb82f44d4e8102
Mikael <mkl.koskela@gmail.com>
2024-01-26 09:17:07 +0000

    public removed

--- apps/pedia/app/pedia/AuthWrapper.tsx
+++ apps/pedia/app/pedia/AuthWrapper.tsx
@@ -10,12 +10,11 @@ export default function AuthWrapper() {
 
   return (
     <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_REACT_APP_AUTH0_DOMAIN as string}
-      clientId={process.env.NEXT_PUBLIC_REACT_APP_AUTH0_CLIENT_ID as string}
+      domain={process.env.NEXT_REACT_APP_AUTH0_DOMAIN as string}
+      clientId={process.env.NEXT_REACT_APP_AUTH0_CLIENT_ID as string}
       authorizationParams={{
         audience: "https://api-gateway-authorizer",
-        redirect_uri: process.env
-          .NEXT_PUBLIC_REACT_APP_AUTH0_REDIRECT_URL as string,
+        redirect_uri: process.env.NEXT_REACT_APP_AUTH0_REDIRECT_URL as string,
       }}
       useRefreshTokens={true}
       useRefreshTokensFallback={true}
