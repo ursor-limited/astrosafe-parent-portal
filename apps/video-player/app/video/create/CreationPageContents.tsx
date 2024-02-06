71b812623cbf4bf5b2cbaf8608d22bce926bf991
Mikael <mkl.koskela@gmail.com>
2024-02-06 09:33:28 +0000

    testing removing mixpanel init ffrom one file

--- apps/video-player/app/video/create/CreationPageContents.tsx
+++ apps/video-player/app/video/create/CreationPageContents.tsx
@@ -22,7 +22,7 @@ import { useWindowSize } from "usehooks-ts";
 import { MAGICAL_BORDER_THICKNESS } from "@/app/v/[videoId]/VideoPageContents";
 import { useAuth0 } from "@auth0/auth0-react";
 import SignupPromptDialog from "@/app/components/SignupPromptDialog";
-import mixpanel from "mixpanel-browser";
+//import mixpanel from "mixpanel-browser";
 
 const Player = dynamic(
   () => import("@/app/components/player"),
@@ -82,9 +82,9 @@ function CreationPageContents(props: { details: IVideo }) {
   // }, []);
 
   const { user } = useAuth0();
-  useEffect(() => {
-    user?.email && mixpanel.identify(user?.email);
-  }, [user?.email]);
+  // useEffect(() => {
+  //   user?.email && mixpanel.identify(user?.email);
+  // }, [user?.email]);
 
   const [playing, setPlaying] = useState<boolean>(false);
   const [description, setDescription] = useState<string>("");
@@ -153,7 +153,7 @@ function CreationPageContents(props: { details: IVideo }) {
   const router = useRouter();
   const submit = () => {
     setLoading(true);
-    mixpanel.track("video created");
+    //mixpanel.track("video created");
     ApiController.createVideo({
       title,
       description,
@@ -293,9 +293,9 @@ function CreationPageContents(props: { details: IVideo }) {
                           if (user) {
                             submit();
                           } else {
-                            mixpanel.track(
-                              "creation page - opened signup prompt dialog"
-                            );
+                            // mixpanel.track(
+                            //   "creation page - opened signup prompt dialog"
+                            // );
                             setSignupPromptDialogOpen(true);
                           }
                         }}
