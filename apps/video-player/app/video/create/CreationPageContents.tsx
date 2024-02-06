fa04f8a767b0ed76ed09a6a61dbf09fcc00f1c17
Mikael <mkl.koskela@gmail.com>
2024-02-06 09:30:12 +0000

    testing removing mixpanel init ffrom one file

--- apps/video-player/app/video/create/CreationPageContents.tsx
+++ apps/video-player/app/video/create/CreationPageContents.tsx
@@ -65,21 +65,21 @@ const CreationPageInputSection = (props: {
 const extractUrl = (html: string) => html.split('src="')[1].split("?")[0];
 
 function CreationPageContents(props: { details: IVideo }) {
-  useEffect(
-    () =>
-      mixpanel.init(
-        process.env.NEXT_PUBLIC_REACT_APP_MIXPANEL_PROJECT_TOKEN as string,
-        {
-          debug: true,
-          track_pageview: false,
-          persistence: "localStorage",
-        }
-      ),
-    []
-  );
-  useEffect(() => {
-    mixpanel?.track("creation page");
-  }, []);
+  // useEffect(
+  //   () =>
+  //     mixpanel.init(
+  //       process.env.NEXT_PUBLIC_REACT_APP_MIXPANEL_PROJECT_TOKEN as string,
+  //       {
+  //         debug: true,
+  //         track_pageview: false,
+  //         persistence: "localStorage",
+  //       }
+  //     ),
+  //   []
+  // );
+  // useEffect(() => {
+  //   mixpanel?.track("creation page");
+  // }, []);
 
   const { user } = useAuth0();
   useEffect(() => {
