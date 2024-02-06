1dc4d4f2734beb87c1ca8f85508350e1be99c427
ursorApp <ursor.app@gmail.com>
2024-02-06 12:36:11 +0000

    test

---
+++ apps/video-player/app/robots.ts
@@ -0,0 +1,28 @@
+export default function handler(req: any, res: any) {
+    if (process.env.VERCEL_ENV === "production") {
+        const robotsProd = `
+        # *
+        User-agent: *
+        Allow: /
+
+        # Host
+        Host: https://www.astrosafe.co
+
+        # Sitemaps
+        Sitemap: https://astrosafe.co/sitemap.xml
+        `;
+        res.send(robotsProd);
+        return
+    } else {
+        const robotsDev = `
+        # *
+        User-agent: *
+        Disallow: /
+
+        # Host
+        Host: https://dev.astrosafe.co
+        `;
+        res.send(robotsDev);
+        return
+    }
+}
