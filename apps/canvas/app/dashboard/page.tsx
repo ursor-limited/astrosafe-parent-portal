7a6f824ff737081cc9e2278cbf9b881d4f2ee628
Mikael <mkl.koskela@gmail.com>
2024-03-05 08:31:44 +0000

    ...

--- apps/canvas/app/dashboard/page.tsx
+++ apps/canvas/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import AuthWrapper from "../components/AuthWrapper";
+import AuthWrapper from "../components/AuthWrapper";
 import DashboardPageContents from "./DashboardPageContents";
 import landingPageDetails from "../landing/[urlId]/jsons";
 import {} from "@/app/worksheet/[id]/EquationWorksheet";
@@ -7,10 +7,10 @@ import Head from "next/head";
 import { UserProvider } from "../components/UserContext";
 import dynamic from "next/dynamic";
 
-const AuthWrapper = dynamic(
-  () => import("../components/AuthWrapper"),
-  { ssr: false } // not including this component on server-side due to its dependence on 'document'
-);
+// const AuthWrapper = dynamic(
+//   () => import("../components/AuthWrapper"),
+//   { ssr: false } // not including this component on server-side due to its dependence on 'document'
+// );
 
 async function DashboardPage({ params }: { params: { urlId: string } }) {
   //const details = landingPageDetails?.find((l) => l.urlId === params.urlId);
