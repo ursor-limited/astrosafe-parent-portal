335bb82e4b25a5301c8baacbb983fc65b07f25a3
Mikael <mkl.koskela@gmail.com>
2024-03-08 10:17:09 +0000

    init

---
+++ apps/header/app/layout.tsx
@@ -0,0 +1,57 @@
+import { Rubik } from "next/font/google";
+import "./globals.css";
+import { Stack } from "@mui/system";
+import Background from "@/images/background.png";
+import { Metadata } from "next";
+import { PALETTE } from "ui";
+import NotificationProvider from "./components/NotificationProvider";
+import UrsorNotificationBar from "./components/UrsorNotificationBar";
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: "variable",
+});
+
+// export const metadata: Metadata = {
+//   title: "SafeTube - A Safe video player for teachers and schools",
+//   description:
+//     "The safe video player for classrooms to access youtube and vimeo videos safely. Students get access to distraction free video links.",
+// };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" style={{ zIndex: 999999999999 }}>
+      <meta name="theme-color" content={PALETTE.primary.navy} />
+      <body
+        className={rubik.className}
+        style={{
+          margin: 0,
+        }}
+      >
+        <Stack
+          height="100vh"
+          minHeight="100vh"
+          overflow="scroll"
+          width="100vw"
+          sx={{
+            backgroundImage: `url(${Background.src})`,
+            backgroundSize: "cover",
+            boxSizing: "border-box",
+          }}
+          position="relative"
+        >
+          <NotificationProvider>
+            <Stack width="100%" justifyContent="center" zIndex={999999999}>
+              <UrsorNotificationBar />
+            </Stack>
+            {children}
+          </NotificationProvider>
+        </Stack>
+      </body>
+    </html>
+  );
+}
