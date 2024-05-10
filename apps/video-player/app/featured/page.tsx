efe2525c17618970e3855848eb7a55b228ce7a58
Mikael <mkl.koskela@gmail.com>
2024-05-10 10:36:40 +0100

    ....

---
+++ apps/video-player/app/featured/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import featured from "./featured.json";
+import { Metadata } from "next";
+import { IAstroLandingPage } from "../tools/multiplication-chart/[urlId]/LandingPageContents";
+
+export const dynamic = "force-static"; // for SEO, as explained in https://github.com/vercel/next.js/discussions/57644#discussioncomment-8638432
+
+export const metadata: Metadata = {
+  title: "AstroSafe's Featured Lessons",
+  description: "AstroSafe's Featured Lessons",
+  openGraph: {
+    images:
+      "https://ursorassets.s3.eu-west-1.amazonaws.com/Frame_427320551.webp",
+    title: "AstroSafe's Featured Lessons",
+    description: "AstroSafe's Featured Lessons",
+  },
+};
+
+async function FeaturedLessonsPage() {
+  const details: IAstroLandingPage = {
+    ...featured,
+  };
+  return <FeaturedLessonsPageContents {...details} />;
+}
+
+export default FeaturedLessonsPage;
