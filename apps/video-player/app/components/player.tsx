070489b6769b1a40f0a5acdb3514e9fc483ee939
Mikael <mkl.koskela@gmail.com>
2024-05-24 13:24:35 +0100

    ....

--- apps/video-player/app/components/player.tsx
+++ apps/video-player/app/components/player.tsx
@@ -120,9 +120,11 @@ const Player = (props: {
     } else {
       //@ts-ignore
       const playah = new Vimeo.Player(props.playerId);
-      playah.on("pause", () => setPlaying(false));
-      playah.on("play", () => setPlaying(true));
-      setPlayer(playah);
+      if (playah) {
+        playah.on("pause", () => setPlaying(false));
+        playah.on("play", () => setPlaying(true));
+        setPlayer(playah);
+      }
     }
     setPlaying(false);
   }
@@ -269,17 +271,15 @@ const Player = (props: {
     setEnded(false);
     if (
       url?.includes("youtube") &&
-      ((player?.v || player?.playerInfo).playerState === 2 ||
-        (player?.v || player?.playerInfo).playerState === 0 || // 0 is the ended
-        (player?.v || player?.playerInfo).playerState === 5) // 5 is the non-yet-started
+      ((player?.v || player?.playerInfo)?.playerState === 2 ||
+        (player?.v || player?.playerInfo)?.playerState === 0 || // 0 is the ended
+        (player?.v || player?.playerInfo)?.playerState === 5) // 5 is the non-yet-started
     ) {
       player?.playVideo();
     } else if (url?.includes("vimeo")) {
       player?.play();
     }
   };
-
-  console.log(player);
 
   const [fullScreen, setFullScreen] = useState<boolean>(false);
   useEffect(() => props.setFullscreen?.(fullScreen), [fullScreen]);
