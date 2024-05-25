29721e95aea043cf501dbea51be1422673975be0
Mikael <mkl.koskela@gmail.com>
2024-05-25 12:10:34 +0100

    making input

--- apps/aw/app/form/FormPage.tsx
+++ apps/aw/app/form/FormPage.tsx
@@ -1,11 +1,36 @@
 "use client";
 
+import { useState } from "react";
+
 const BG_COLOR = "#F8F8F8";
 
-export default function FormPage() {
+export function AWInputField(props: {
+  value: string;
+  setValue: (newValue: string) => void;
+}) {
   return (
-    <div className="h-screen w-screen bg-background-primary flex justify-center items-center width">
-      <div className="bg-greyscale-white w-[1050px] h-[706px] border-2 border-solid border-greyscale-6"></div>
+    <div className="h-[50px] w-full flex items-center px-lg bg-components-fields-fieldBg rounded-xs">
+      <input
+        className="text-base/[18px] bg-transparent placeholder-greyscale-6"
+        placeholder="Boo"
+        value={props.value}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          props.setValue(event.target.value)
+        }
+      />
+    </div>
+  );
+}
+
+export default function FormPage() {
+  const [value, setValue] = useState<string>("");
+  return (
+    <div className="h-screen w-screen bg-background-primary flex justify-center items-center">
+      <div className="bg-greyscale-white w-[1050px] h-[706px] border-2 border-solid border-greyscale-6 flex justify-center items-center">
+        <div className="w-[600px] flex flex-col gap-32">
+          <AWInputField value={value} setValue={setValue} />
+        </div>
+      </div>
     </div>
   );
 }
--- apps/aw/tailwind.config.js
+++ apps/aw/tailwind.config.js
@@ -5,13 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    padding: {
+      lg: '12px'
+    },
+    borderRadius: {
+      xs: '4px'
+    },
     colors: {
+      transparent: "transparent",
       background: {
         primary: "#F8F8F8",
       },
       greyscale: {
         white: "#FFFFFF",
         6: "#B6C6C6",
+      },
+      components: {
+        fields: {
+          fieldBg: "#F0F1F1",
+        },
       },
     },
     extend: {},
