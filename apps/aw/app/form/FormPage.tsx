5d1794518ae7deca36326b6a23cd599d0daa5f45
Mikael <mkl.koskela@gmail.com>
2024-05-25 13:12:16 +0100

    map form

--- apps/aw/app/form/FormPage.tsx
+++ apps/aw/app/form/FormPage.tsx
@@ -2,17 +2,53 @@
 
 import { useState } from "react";
 
-const BG_COLOR = "#F8F8F8";
+type AWFormItemInput = "text" | "dropdown" | "multiChoice" | "phoneNumber";
 
-export function AWInputField(props: {
+interface IAWFormItem {
+  id: string;
+  title: string;
+  inputType: AWFormItemInput;
+  placeholder?: string;
+}
+
+interface IAWFormItemAnswer {
+  id: string;
   value: string;
+}
+
+const ITEMS: IAWFormItem[][] = [
+  [
+    {
+      id: "6651d2bb1aaa5843d82bc607",
+      title: "Full name",
+      inputType: "text",
+      placeholder: "Insert name here",
+    },
+    {
+      id: "6651d2d30bc6c109d2a97aed",
+      title: "Job title",
+      inputType: "text",
+      placeholder: "Insert title of role played in organization",
+    },
+    {
+      id: "6651d2db9af2d8a25e707374",
+      title: "Email",
+      inputType: "text",
+      placeholder: "Insert email address here",
+    },
+  ],
+];
+
+export function AWTextField(props: {
+  value?: string;
   setValue: (newValue: string) => void;
+  placeholder?: string;
 }) {
   return (
     <div className="h-[50px] w-full flex items-center px-lg bg-fields-bg rounded-xs">
       <input
-        className="text-base/[18px] bg-transparent placeholder-greyscale-6 text-fields-text-pressed placeholder:text-fields-text-placeholder"
-        placeholder="Boo"
+        className="w-full text-base/[18px] bg-transparent placeholder-greyscale-6 text-fields-text-pressed placeholder:text-fields-text-placeholder"
+        placeholder={props.placeholder}
         value={props.value}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
           props.setValue(event.target.value)
@@ -22,28 +58,49 @@ export function AWInputField(props: {
   );
 }
 
-export function AWFormItem(props: {
-  n: number;
-  title: string;
-  children: React.ReactNode;
-}) {
+export function AWFormItem(
+  props: IAWFormItem & {
+    i: number;
+    value?: string;
+    setValue: (newValue: string) => void;
+  }
+) {
   return (
-    <div className="flex flex-col gap-spacing-1">
-      <div className="font-medium">{`${props.n}) ${props.title}`}</div>
-      {props.children}
+    <div className="flex flex-col gap-1">
+      <div className="font-medium">{`${props.i}) ${props.title}`}</div>
+      {props.inputType === "text" ? (
+        <AWTextField
+          value={props.value}
+          setValue={props.setValue}
+          placeholder={props.placeholder}
+        />
+      ) : null}
     </div>
   );
 }
 
 export default function FormPage() {
-  const [value, setValue] = useState<string>("");
+  const [step, setStep] = useState<number>(0);
+  const [answers, setAnswers] = useState<IAWFormItemAnswer[]>([]);
   return (
     <div className="h-screen w-screen bg-background-primary flex justify-center items-center">
       <div className="bg-greyscale-white w-[1050px] h-[706px] border-2 border-solid border-greyscale-6 flex justify-center items-center">
-        <div className="w-[600px] flex flex-col gap-spacing-xl">
-          <AWFormItem n={1} title="Full name">
-            <AWInputField value={value} setValue={setValue} />
-          </AWFormItem>
+        <div className="w-[600px] flex flex-col gap-xl">
+          {ITEMS[step].map((item, i) => (
+            <AWFormItem
+              key={item.id}
+              {...item}
+              i={i + 1}
+              value={answers.find((a) => a.id === item.id)?.value}
+              setValue={(newValue) =>
+                setAnswers((prev) =>
+                  prev.map((a) =>
+                    a.id === item.id ? { ...a, value: newValue } : a
+                  )
+                )
+              }
+            />
+          ))}
         </div>
       </div>
     </div>
