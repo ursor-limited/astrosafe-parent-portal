6d483cad0b5916460fd559e1a5636b125e58d297
Mikael <mkl.koskela@gmail.com>
2024-06-25 10:15:48 +0100

    fix little

--- apps/browser/app/searchResults/SearchResultsPageContents.tsx
+++ apps/browser/app/searchResults/SearchResultsPageContents.tsx
@@ -16,7 +16,6 @@ import PageLayout from "../components/PageLayout";
 import BrowserLinkCard from "../components/BrowserLinkCard";
 import { BrowserContent, IBrowserContent } from "../home/AstroContentColumns";
 import VideoCard from "../components/VideoCard";
-import { Router } from "next/router";
 import { useRouter } from "next/navigation";
 
 const DUMMY_VIDEOS = [
@@ -88,7 +87,7 @@ const AstroContentRow = (props: {
       ),
     ];
     setAllContentDetails(all);
-  });
+  }, [props.links, props.videos]);
 
   const router = useRouter();
 
@@ -96,20 +95,23 @@ const AstroContentRow = (props: {
     <Stack direction="row" spacing="12px">
       {[
         <Stack key="lp" minWidth="8px" />,
-        ...allContentDetails.map((c) => (
-          <Stack
-            key={c.details.id}
-            onClick={() => router.push(c.details.url)}
-            minWidth="350px"
-            width="350px"
-          >
-            {c.type === "link" ? (
-              <BrowserLinkCard link={c.details as IBrowserLink} />
-            ) : c.type === "video" ? (
-              <VideoCard video={c.details as IVideo} />
-            ) : null}
-          </Stack>
-        )),
+        ...allContentDetails.map((c) => {
+          console.log(c.details.url);
+          return (
+            <Stack
+              key={c.details.id}
+              onClick={() => router.push(c.details.url)}
+              minWidth="350px"
+              width="350px"
+            >
+              {c.type === "link" ? (
+                <BrowserLinkCard link={c.details as IBrowserLink} />
+              ) : c.type === "video" ? (
+                <VideoCard video={c.details as IVideo} />
+              ) : null}
+            </Stack>
+          );
+        }),
         <Stack key="rp" minWidth="8px" />,
       ]}
     </Stack>
