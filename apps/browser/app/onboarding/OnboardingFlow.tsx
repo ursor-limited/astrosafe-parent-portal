99764b88707f288d8b07c5b017c6ff916a0fd3a1
Mikael <mkl.koskela@gmail.com>
2024-07-17 17:41:12 +0100

    made security

--- apps/browser/app/onboarding/OnboardingFlow.tsx
+++ apps/browser/app/onboarding/OnboardingFlow.tsx
@@ -8,6 +8,8 @@ import GlobeIcon from "@/images/icons/GlobeIcon.svg";
 import ChevronRightIcon from "@/images/icons/ChevronRight.svg";
 import ArrowLeftIcon from "@/images/icons/ArrowLeftIcon.svg";
 import UrsorParticles from "../components/UrsorParticles";
+import { useState } from "react";
+import _ from "lodash";
 
 const ConfigurationStepCard = (props: {
   n: number;
@@ -47,13 +49,154 @@ const ConfigurationStepCard = (props: {
   </Stack>
 );
 
+const ConfigurationStepButton = (props: {
+  text: string;
+  selected: boolean;
+}) => (
+  <Stack
+    height="32px"
+    borderRadius="32px"
+    border="2px solid rgba(255,255,255,0.88)"
+    px="12px"
+    justifyContent="center"
+    alignItems="center"
+    sx={{
+      opacity: props.selected ? 1 : 0.6,
+    }}
+  >
+    <Typography variant="small" bold color="rgba(255,255,255,0.88)">
+      {props.text}
+    </Typography>
+  </Stack>
+);
+
+const AgeCard = (props: {
+  title: string;
+  ages: string;
+  selected: boolean;
+  faded: boolean;
+  subtitle: string;
+  onClick: () => void;
+}) => (
+  <Stack
+    width="251px"
+    height="317px"
+    borderRadius="12px"
+    alignItems="center"
+    p="16px"
+    boxSizing="border-box"
+    bgcolor="rgba(255,255,255,0.95)"
+    sx={{
+      opacity: props.faded ? 0.6 : 1,
+      cursor: "pointer",
+      "&:hover": { opacity: 0.8 },
+      transition: "0.2s",
+      pointerEvents: props.selected ? "none" : undefined,
+      outline: `2px solid ${
+        props.selected ? PALETTE.secondary.purple[2] : "transparent"
+      }`,
+    }}
+    spacing="12px"
+    onClick={props.onClick}
+    boxShadow={`0 0 30px ${props.selected ? "#A594FF" : undefined}`}
+  >
+    <Typography variant="h4" color={PALETTE.secondary.purple[2]}>
+      {props.title}
+    </Typography>
+    <Typography bold variant="small" color={PALETTE.secondary.grey[5]}>
+      {props.ages}
+    </Typography>
+    <Typography bold variant="small" color={PALETTE.secondary.grey[5]}>
+      {props.subtitle}
+    </Typography>
+  </Stack>
+);
+
+const INTRO_STEP_TITLE = "Lets configure your browser in 3 steps";
+const IntroStepView = () => (
+  <Stack
+    flex={1}
+    sx={{
+      svg: {
+        path: {
+          fill: PALETTE.secondary.purple[1],
+        },
+      },
+    }}
+    direction="row"
+    spacing="18px"
+    alignItems="center"
+  >
+    <ConfigurationStepCard
+      icon={ShieldLockIcon}
+      n={1}
+      title="Configure your security settings"
+    />
+    <ChevronRightIcon height="32px" width="32px" />
+    <ConfigurationStepCard
+      icon={VerifiedIcon}
+      n={2}
+      title="Select your approved Content"
+    />
+    <ChevronRightIcon height="32px" width="32px" />
+    <ConfigurationStepCard
+      icon={GlobeIcon}
+      n={3}
+      title="Set up your personal Browser"
+    />
+  </Stack>
+);
+
+const SECURITY_STEP_TITLE = "Set up the Browser, the basics...";
+const SecurityStepView = () => {
+  const [selectedCardIndex, setSelectedCardIndex] = useState<
+    number | undefined
+  >();
+  return (
+    <Stack direction="row" spacing="32px">
+      <AgeCard
+        title="Adventurer"
+        ages="For age 3 to 6"
+        subtitle="Provide access to a safe version of the internet with safe links."
+        selected={selectedCardIndex === 0}
+        faded={_.isNumber(selectedCardIndex) && selectedCardIndex !== 0}
+        onClick={() => setSelectedCardIndex(0)}
+      />
+      <AgeCard
+        title="Explorer"
+        ages="For age 7 to 10"
+        subtitle="Provide access to a safe version of the internet with safe links."
+        selected={selectedCardIndex === 1}
+        faded={_.isNumber(selectedCardIndex) && selectedCardIndex !== 1}
+        onClick={() => setSelectedCardIndex(1)}
+      />
+      <AgeCard
+        title="Navigator"
+        ages="For ages 11+"
+        subtitle="Provide access to a safe version of the internet with safe links."
+        selected={selectedCardIndex === 2}
+        faded={_.isNumber(selectedCardIndex) && selectedCardIndex !== 2}
+        onClick={() => setSelectedCardIndex(2)}
+      />
+    </Stack>
+  );
+};
+
+const CONTENT_STEP_VIEWS: { title: string; component: React.ReactNode }[] = [
+  {
+    title: "",
+    component: <Stack />,
+  },
+];
+
 export default function OnBoardingFlow(props: { mobile: boolean }) {
+  const [step, setStep] = useState<number | undefined>(0);
   return (
     <>
       <Stack
         width="100%"
         height="100%"
-        justifyContent="center"
+        justifyContent="space-between"
         alignItems="center"
         p="40px"
         pb="67px"
@@ -61,7 +204,13 @@ export default function OnBoardingFlow(props: { mobile: boolean }) {
         spacing="30px"
         zIndex={2}
       >
-        <Stack width="100%" height="80px" justifyItems="center">
+        <Stack
+          width="100%"
+          height="80px"
+          justifyContent="space-between"
+          direction="row"
+          alignItems="center"
+        >
           <Stack
             height="48px"
             width="48px"
@@ -77,6 +226,34 @@ export default function OnBoardingFlow(props: { mobile: boolean }) {
           >
             <ArrowLeftIcon height="26px" width="26px" />
           </Stack>
+          <Stack
+            direction="row"
+            spacing="8px"
+            alignItems="center"
+            sx={{
+              svg: {
+                path: {
+                  fill: "rgba(255,255,255,0.65)",
+                },
+              },
+            }}
+          >
+            <ConfigurationStepButton
+              text="1. Security"
+              selected={_.isNumber(step) && step >= 0}
+            />
+            <ChevronRightIcon height="32px" width="32px" />
+            <ConfigurationStepButton
+              text="2. Content"
+              selected={_.isNumber(step) && step >= 1}
+            />
+            <ChevronRightIcon height="32px" width="32px" />
+            <ConfigurationStepButton
+              text="3. Curation"
+              selected={_.isNumber(step) && step === 2}
+            />
+          </Stack>
+          <Stack width="48px" />
         </Stack>
         <Stack width="523px">
           <Typography
@@ -84,40 +261,20 @@ export default function OnBoardingFlow(props: { mobile: boolean }) {
             variant="h3"
             sx={{ textAlign: "center" }}
           >
-            Lets configure your browser in 3 steps
+            {!_.isNumber(step)
+              ? INTRO_STEP_TITLE
+              : step === 0
+              ? SECURITY_STEP_TITLE
+              : ""}
           </Typography>
         </Stack>
-        <Stack
-          flex={1}
-          sx={{
-            svg: {
-              path: {
-                fill: PALETTE.secondary.purple[1],
-              },
-            },
-          }}
-          direction="row"
-          spacing="18px"
-          alignItems="center"
-        >
-          <ConfigurationStepCard
-            icon={ShieldLockIcon}
-            n={1}
-            title="Configure your security settings"
-          />
-          <ChevronRightIcon height="32px" width="32px" />
-          <ConfigurationStepCard
-            icon={VerifiedIcon}
-            n={2}
-            title="Select your approved Content"
-          />
-          <ChevronRightIcon height="32px" width="32px" />
-          <ConfigurationStepCard
-            icon={GlobeIcon}
-            n={3}
-            title="Set up your personal Browser"
-          />
-        </Stack>
+        {!_.isNumber(step) ? (
+          <IntroStepView />
+        ) : step === 0 ? (
+          <SecurityStepView />
+        ) : (
+          ""
+        )}
         <UrsorButton
           dark
           variant="tertiary"
