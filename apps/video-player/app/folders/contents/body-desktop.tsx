fcadce3ce21cc7bb29b245b650fe503269fd9cc9
Mikael <mkl.koskela@gmail.com>
2024-08-02 12:56:48 +0100

    empoty state folders

--- apps/video-player/app/folders/contents/body-desktop.tsx
+++ apps/video-player/app/folders/contents/body-desktop.tsx
@@ -6,6 +6,8 @@ import { Stack } from "@mui/system";
 import { useRouter } from "next/navigation";
 import { IEnrichedContentBucket } from "./common";
 import FolderCard from "@/app/components/FolderCard";
+import { EmptyStateIllustration } from "@/app/components/EmptyStateIllustration";
+import { PALETTE, Typography } from "ui";
 
 const AllFoldersPageDesktopBody = (props: {
   folders: IEnrichedContentBucket[];
@@ -27,20 +29,26 @@ const AllFoldersPageDesktopBody = (props: {
       maxWidth={834}
       scrollable
     >
-      <Stack pt="20px" pl="51px">
-        <DynamicCardGrid cardWidth="292px" rowGap="40px" columnGap="20px">
-          {props.folders.map((f, i) => (
-            <UrsorFadeIn key={f.id} duration={800} delay={i * 90}>
-              <FolderCard
-                {...f}
-                clickCallback={() => router.push(`/folders/${f.id}`)}
-                editingCallback={props.onUpdate}
-                deletionCallback={props.onUpdate}
-              />
-            </UrsorFadeIn>
-          ))}
-        </DynamicCardGrid>
-      </Stack>
+      {props.folders.length > 0 ? (
+        <Stack pt="20px" pl="51px">
+          <DynamicCardGrid cardWidth="292px" rowGap="40px" columnGap="20px">
+            {props.folders.map((f, i) => (
+              <UrsorFadeIn key={f.id} duration={800} delay={i * 90}>
+                <FolderCard
+                  {...f}
+                  clickCallback={() => router.push(`/folders/${f.id}`)}
+                  editingCallback={props.onUpdate}
+                  deletionCallback={props.onUpdate}
+                />
+              </UrsorFadeIn>
+            ))}
+          </DynamicCardGrid>
+        </Stack>
+      ) : (
+        <EmptyStateIllustration paddingTop={20}>
+          No Folders yet
+        </EmptyStateIllustration>
+      )}
     </PageLayout>
   );
 };
