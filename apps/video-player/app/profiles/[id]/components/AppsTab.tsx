a26efe4ca02394cea3a7df63f1b8d1196a942dcc
Mikael <mkl.koskela@gmail.com>
2024-08-08 12:14:15 +0100

    added pagination to apps

--- apps/video-player/app/profiles/[id]/components/AppsTab.tsx
+++ apps/video-player/app/profiles/[id]/components/AppsTab.tsx
@@ -1,14 +1,15 @@
 import DynamicCardGrid from "@/app/components/DynamicCardGrid";
 import DesktopIcon from "@/images/icons/DesktopIcon.svg";
 import PlusIcon from "@/images/icons/PlusIcon.svg";
-import CheckIcon from "@/images/icons/CheckIcon.svg";
+import ChevronLeftIcon from "@/images/icons/ChevronLeft.svg";
+import ChevronRightIcon from "@/images/icons/ChevronRight.svg";
 import { AstroBentoCard } from "../../../filters/[id]/components/AstroBentoCard";
 import { FilterLegend } from "../../../filters/[id]/components/CategoriesSection";
 import AppToggleCard from "../components/AppToggleCard";
 import { Stack } from "@mui/system";
 import Image from "next/image";
 import UrsorFadeIn from "@/app/components/UrsorFadeIn";
-import { PALETTE, UrsorButton } from "ui";
+import { PALETTE, Typography, UrsorButton } from "ui";
 import { IFilterUrl } from "../../../filters/contents/common";
 import ProfilePageTabLayout from "./ProfilePageTabLayout";
 import { IDevice } from "@/app/filters/[id]/contents/common";
@@ -18,6 +19,7 @@ import AstroCard from "@/app/filters/[id]/components/AstroCard";
 import _ from "lodash";
 
 const PAGE_SIZE = 20;
+const N_PAGES = 6;
 
 export interface IAppCategory {
   id: number;
@@ -50,7 +52,7 @@ const DevicePageAppsTab = (props: { deviceId: IDevice["id"] }) => {
     >
       <Stack pb="32px">
         <AstroCard>
-          <Stack p="16px">
+          <Stack px="16px" pt="16px" justifyContent="center">
             <DynamicCardGrid cardWidth="292px" rowGap="8px" columnGap="20px">
               {apps.map((a, i) => (
                 <UrsorFadeIn key={a.id} duration={800} delay={i * 80}>
@@ -72,6 +74,67 @@ const DevicePageAppsTab = (props: { deviceId: IDevice["id"] }) => {
                 </UrsorFadeIn>
               ))}
             </DynamicCardGrid>
+            <Stack
+              direction="row"
+              spacing="22px"
+              alignItems="center"
+              justifyContent="center"
+              py="20px"
+            >
+              {[
+                <Stack
+                  key="left"
+                  sx={{
+                    cursor: "pointer",
+                    transition: "0.2s",
+                    "&:hover": { opacity: 0.7 },
+                    pointerEvents: pageIndex === 0 ? "none" : undefined,
+                    opacity: pageIndex === 0 ? 0.4 : 1,
+                  }}
+                  onClick={() => setPageIndex(pageIndex - 1)}
+                >
+                  <ChevronLeftIcon height="15px" width="15px" />
+                </Stack>,
+                ...[...Array(N_PAGES).keys()].map((i) => (
+                  <Stack
+                    key={i}
+                    sx={{
+                      cursor: "pointer",
+                      transition: "0.2s",
+                      "&:hover": { opacity: 0.7 },
+                      pointerEvents: pageIndex === i ? "none" : undefined,
+                    }}
+                    onClick={() => setPageIndex(i)}
+                  >
+                    <Typography
+                      bold
+                      sx={{ fontSize: 14 }}
+                      color={
+                        i === pageIndex
+                          ? PALETTE.secondary.purple[2]
+                          : PALETTE.secondary.grey[3]
+                      }
+                    >
+                      {i + 1}
+                    </Typography>
+                  </Stack>
+                )),
+                <Stack
+                  key="right"
+                  sx={{
+                    cursor: "pointer",
+                    transition: "0.2s",
+                    "&:hover": { opacity: 0.7 },
+                    pointerEvents:
+                      pageIndex === N_PAGES - 1 ? "none" : undefined,
+                    opacity: pageIndex === N_PAGES - 1 ? 0.4 : 1,
+                  }}
+                  onClick={() => setPageIndex(pageIndex + 1)}
+                >
+                  <ChevronRightIcon height="15px" width="15px" />
+                </Stack>,
+              ]}
+            </Stack>
           </Stack>
         </AstroCard>
       </Stack>
