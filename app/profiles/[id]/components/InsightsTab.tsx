383697f59da62c496f78166186ca139b6b3c1521
Mikael <mkl.koskela@gmail.com>
2024-08-26 17:12:27 +0100

    ...

--- app/profiles/[id]/components/InsightsTab.tsx
+++ app/profiles/[id]/components/InsightsTab.tsx
@@ -1,21 +1,19 @@
-import { Stack } from '@mui/system';
-import { PALETTE, Typography, UrsorButton } from '@/ui';
-import ChevronRightIcon from '@/images/icons/ChevronRight.svg';
-import ChevronLeftIcon from '@/images/icons/ChevronLeft.svg';
-import { AstroBentoCard } from '@/app/filters/[id]/components/AstroBentoCard';
-import _ from 'lodash';
-import AstroTimeChart from './AstroTimeChart';
-import { useEffect, useState } from 'react';
-import dayjs from 'dayjs';
-import advancedFormat from 'dayjs/plugin/advancedFormat.js';
-import HistorySection, { IHistoryItem } from './HistorySection';
-import CalendarButton from '@/app/components/CalendarButton';
-import { IFilterDomain, IFilterUrl } from '@/app/filters/contents/common';
-import { DUMMY_GROUP_ID } from '@/app/filters/contents/body-mobile';
-import ApiController from '@/app/api';
-import MostVisitedSitesSection from './MostVisitedSitesSection';
-import { IDevice } from '@/app/filters/[id]/contents/common';
-import { useWindowSize } from 'usehooks-ts';
+import { Stack } from "@mui/system";
+import { Typography } from "@/ui";
+import ChevronRightIcon from "@/images/icons/ChevronRight.svg";
+import ChevronLeftIcon from "@/images/icons/ChevronLeft.svg";
+import { AstroBentoCard } from "@/app/filters/[id]/components/AstroBentoCard";
+import _ from "lodash";
+import AstroTimeChart from "./AstroTimeChart";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat.js";
+import HistorySection from "./HistorySection";
+import CalendarButton from "@/app/components/CalendarButton";
+import ApiController from "@/app/api";
+import MostVisitedSitesSection from "./MostVisitedSitesSection";
+import { IDevice } from "@/app/filters/[id]/contents/common";
+import { useWindowSize } from "usehooks-ts";
 dayjs.extend(advancedFormat);
 
 const SWITCH_TO_COLUMN_WINDOW_WIDTH_THRESHOLD = 1260;
@@ -27,90 +25,13 @@ export interface IVisitedSite {
   screenTime: number;
 }
 
-export const DUMMY_DOMAIN_URLS: (IFilterDomain & { time: number })[] = [
-  {
-    id: 1,
-    domain: 'https://www.nationalreview.com/buuuuu/boooo',
-    title: 'National Review',
-    faviconUrl:
-      'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-    time: 3094,
-    urls: [
-      {
-        id: 1,
-        url: 'https://www.nationalreview.com/',
-        title: 'National Review',
-        imageUrl:
-          'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-        createdAt: '2024-06-06',
-        groupId: DUMMY_GROUP_ID,
-      },
-      {
-        id: 2,
-        url: 'https://www.nationalgeographic.com/',
-        title: 'National Geographic',
-        imageUrl:
-          'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-        createdAt: '2024-06-06',
-        groupId: DUMMY_GROUP_ID,
-      },
-      {
-        id: 3,
-        url: 'https://www.nintendo.com/',
-        title: 'Nintendo',
-        imageUrl:
-          'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-        createdAt: '2024-06-06',
-        groupId: DUMMY_GROUP_ID,
-      },
-    ],
-  },
-  {
-    id: 2,
-    domain: 'https://www.nationalreview.com/',
-    title: 'National Review',
-    faviconUrl:
-      'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-    time: 124,
-    urls: [
-      {
-        id: 1,
-        url: 'https://www.nationalreview.com/',
-        title: 'National Review',
-        imageUrl:
-          'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-        createdAt: '2024-06-06',
-        groupId: DUMMY_GROUP_ID,
-      },
-      {
-        id: 2,
-        url: 'https://www.nationalgeographic.com/',
-        title: 'National Geographic',
-        imageUrl:
-          'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-        createdAt: '2024-06-06',
-        groupId: DUMMY_GROUP_ID,
-      },
-      {
-        id: 3,
-        url: 'https://www.nintendo.com/',
-        title: 'Nintendo',
-        imageUrl:
-          'https://ursorassets.s3.eu-west-1.amazonaws.com/pingu_profile.jpg',
-        createdAt: '2024-06-06',
-        groupId: DUMMY_GROUP_ID,
-      },
-    ],
-  },
-];
-
 export interface IDayScreenTime {
   date: string;
   screenTime: number;
   timeLimitReached: boolean;
 }
 
-const DevicePageInsightsTab = (props: { deviceId: IDevice['id'] }) => {
+const DevicePageInsightsTab = (props: { deviceId: IDevice["id"] }) => {
   const [times, setTimes] = useState<IDayScreenTime[]>([]);
   const [selectedDayIndex, setSelectedDayIndex] = useState<number>(0); // days from today
   const [rangeEndDayIndex, setRangeEndDayIndex] = useState<number>(0);
@@ -119,8 +40,8 @@ const DevicePageInsightsTab = (props: { deviceId: IDevice['id'] }) => {
   useEffect(() => {
     ApiController.getStats(
       props.deviceId,
-      dayjs().utc().subtract(rangeStartDayIndex, 'days').format('YYYY-MM-DD'),
-      dayjs().utc().subtract(rangeEndDayIndex, 'days').format('YYYY-MM-DD')
+      dayjs().utc().subtract(rangeStartDayIndex, "days").format("YYYY-MM-DD"),
+      dayjs().utc().subtract(rangeEndDayIndex, "days").format("YYYY-MM-DD")
     ).then((stats) => {
       setTimes(stats.screenTime);
       setVisitedSites(
@@ -130,8 +51,8 @@ const DevicePageInsightsTab = (props: { deviceId: IDevice['id'] }) => {
               w.date ===
               dayjs()
                 .utc()
-                .subtract(selectedDayIndex, 'days')
-                .format('YYYY-MM-DD')
+                .subtract(selectedDayIndex, "days")
+                .format("YYYY-MM-DD")
           )?.websites || [],
           (t) => t.screenTime
         )
@@ -148,8 +69,8 @@ const DevicePageInsightsTab = (props: { deviceId: IDevice['id'] }) => {
             t.date ===
             dayjs()
               .utc()
-              .subtract(selectedDayIndex, 'days')
-              .format('YYYY-MM-DD')
+              .subtract(selectedDayIndex, "days")
+              .format("YYYY-MM-DD")
         )?.screenTime ?? 0
       ),
     [times, selectedDayIndex]
@@ -178,56 +99,56 @@ const DevicePageInsightsTab = (props: { deviceId: IDevice['id'] }) => {
   }, [width]);
 
   return (
-    <Stack spacing='24px' pb='32px'>
-      <Stack direction='row' justifyContent='space-between'>
-        <Stack direction='row' spacing='10px' alignItems='center'>
+    <Stack spacing="24px" pb="32px">
+      <Stack direction="row" justifyContent="space-between">
+        <Stack direction="row" spacing="10px" alignItems="center">
           <Stack
             sx={{
-              cursor: 'pointer',
-              transition: '0.2s',
-              '&:hover': { opacity: 0.6 },
+              cursor: "pointer",
+              transition: "0.2s",
+              "&:hover": { opacity: 0.6 },
             }}
             onClick={() => setSelectedDayIndex(selectedDayIndex + 1)}
           >
-            <ChevronLeftIcon height='24px' width='24px' />
+            <ChevronLeftIcon height="24px" width="24px" />
           </Stack>
-          <Typography variant='h5'>
+          <Typography variant="h5">
             {`${
               selectedDayIndex === 0
-                ? 'Today'
+                ? "Today"
                 : selectedDayIndex === 1
-                ? 'Yesterday'
+                ? "Yesterday"
                 : `${dayjs()
                     .utc()
-                    .subtract(selectedDayIndex, 'days')
-                    .format('dddd')}`
+                    .subtract(selectedDayIndex, "days")
+                    .format("dddd")}`
             }, ${dayjs()
               .utc()
-              .subtract(selectedDayIndex, 'days')
-              .format('Do MMMM')}`}
+              .subtract(selectedDayIndex, "days")
+              .format("Do MMMM")}`}
           </Typography>
           <Stack
             sx={{
               opacity: selectedDayIndex === 0 ? 0.3 : 1,
-              pointerEvents: selectedDayIndex === 0 ? 'none' : undefined,
-              cursor: 'pointer',
-              transition: '0.2s',
-              '&:hover': { opacity: 0.6 },
+              pointerEvents: selectedDayIndex === 0 ? "none" : undefined,
+              cursor: "pointer",
+              transition: "0.2s",
+              "&:hover": { opacity: 0.6 },
             }}
             onClick={() => setSelectedDayIndex(selectedDayIndex - 1)}
           >
-            <ChevronRightIcon height='24px' width='24px' />
+            <ChevronRightIcon height="24px" width="24px" />
           </Stack>
         </Stack>
         <CalendarButton
-          value={dayjs().utc().subtract(selectedDayIndex, 'days').toDate()}
+          value={dayjs().utc().subtract(selectedDayIndex, "days").toDate()}
           setValue={(date: Date) =>
-            setSelectedDayIndex(dayjs().diff(date, 'days'))
+            setSelectedDayIndex(dayjs().diff(date, "days"))
           }
         />
       </Stack>
-      <Stack spacing='28px' direction={switchToColumn ? 'column' : 'row'}>
-        <Stack width={switchToColumn ? '100%' : '54%'} height='290px'>
+      <Stack spacing="28px" direction={switchToColumn ? "column" : "row"}>
+        <Stack width={switchToColumn ? "100%" : "54%"} height="290px">
           <AstroBentoCard
             title={`${Math.floor(timeSpent / 60)}h ${Math.floor(
               timeSpent % 60
@@ -235,30 +156,30 @@ const DevicePageInsightsTab = (props: { deviceId: IDevice['id'] }) => {
             notCollapsible
           >
             <Stack
-              mt='33p'
+              mt="33p"
               flex={1}
-              borderRadius='12px'
-              bgcolor='rgb(255,255,255)'
-              py='8px'
-              boxSizing='border-box'
-              spacing='30px'
+              borderRadius="12px"
+              bgcolor="rgb(255,255,255)"
+              py="8px"
+              boxSizing="border-box"
+              spacing="30px"
             >
               {times.length > 0 ? (
                 <AstroTimeChart
                   times={times}
                   selected={dayjs()
                     .utc()
-                    .subtract(selectedDayIndex, 'days')
-                    .format('YYYY-MM-DD')}
+                    .subtract(selectedDayIndex, "days")
+                    .format("YYYY-MM-DD")}
                   setSelectedDatetime={(datetime) =>
-                    setSelectedDayIndex(dayjs().utc().diff(datetime, 'days'))
+                    setSelectedDayIndex(dayjs().utc().diff(datetime, "days"))
                   }
                 />
               ) : null}
             </Stack>
           </AstroBentoCard>
         </Stack>
-        <Stack height='290px' flex={1}>
+        <Stack height="290px" flex={1}>
           <MostVisitedSitesSection sites={visitedSites} />
         </Stack>
       </Stack>
@@ -266,8 +187,8 @@ const DevicePageInsightsTab = (props: { deviceId: IDevice['id'] }) => {
         deviceId={props.deviceId}
         date={dayjs()
           .utc()
-          .subtract(selectedDayIndex, 'days')
-          .format('YYYY-MM-DD')}
+          .subtract(selectedDayIndex, "days")
+          .format("YYYY-MM-DD")}
       />
     </Stack>
   );
