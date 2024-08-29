1846bc9e79727eedc369586fce650e4d5e5b6e16
Tomas Perestrelo <tomasperestrelo21@gmail.com>
2024-08-29 19:38:20 +0100

    Temp changes.

---
+++ rollup.config.mjs
@@ -0,0 +1,40 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import images from 'rollup-plugin-img';
+import css from 'rollup-plugin-import-css';
+import dts from 'rollup-plugin-dts';
+import fs from 'fs';
+
+const loadJSONFile = (file) => {
+  return JSON.parse(fs.readFileSync(file, 'utf-8'));
+};
+
+const packageJson = loadJSONFile('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      css(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
---
+++ src/test.tsx
@@ -0,0 +1,5 @@
+const Test: React.FC = () => {
+  return <div>Hi</div>;
+};
+
+export default Test;
