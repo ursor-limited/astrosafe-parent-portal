c3ca64e9774acee7e9d8e8fdee78d45cd85397a4
Tomas Perestrelo <tomasperestrelo21@gmail.com>
2024-09-04 18:56:44 +0100

    Fix useNavigate hook.

--- src/hooks/useNavigate/index.ts
+++ src/hooks/useNavigate/index.ts
@@ -1,7 +1,6 @@
 const useNavigate = () => {
   const push = (path: string) => {
-    const newUrl =
-      window.location.origin + path.startsWith('/') ? path.substring(1) : path
+    const newUrl = window.location.origin + path
 
     window.location.href = newUrl
   }
